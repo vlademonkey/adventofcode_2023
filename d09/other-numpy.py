1374cd018fc30a5c0db5be1e36a47d7c816f13a6
vlademonkey <vlademonkey@gmail.com>
2023-12-09 19:55:26 -0600

    Day 9 (other)

--- d09/other-numpy.py
+++ d09/other-numpy.py
@@ -1,10 +1,9 @@
 # From: https://www.reddit.com/r/adventofcode/comments/18e5ytd/2023_day_9_solutions/kclqhga/
-
 import numpy as np
 from numpy.polynomial.polynomial import Polynomial
-from pathlib import Path
 
-indata = Path("input.txt").read_text()
+with open("input.txt") as fin:
+    indata = fin.read()
 
 answer = [0, 0]
 for line in indata.splitlines(keepends=False):
@@ -13,3 +12,6 @@ for line in indata.splitlines(keepends=False):
     answer[0] += round(poly(len(y)))
     answer[1] += round(poly(-1))
 print("Part 1: {}\nPart 2: {}".format(*answer))
+
+# From: https://www.reddit.com/r/adventofcode/comments/18e5ytd/2023_day_9_solutions/kco6wzm/
+print(abs(sum(np.diff(np.loadtxt("input.txt", int), n=21, prepend=0, append=0))[::-1]))
